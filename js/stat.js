cf167bda2dccb7d16cc779e63d4df29d52f31772
Vladimir Legotin <wovanza@gmail.com>
2020-01-28 19:49:37 +0500

    Исправил ошибки пробелов

--- js/stat.js
+++ js/stat.js
@@ -18,47 +18,47 @@ var messages = ['Ура вы победили!', 'Список результа�
 var RANDOM_BLUE = 'hsl(240, ' + Math.floor(Math.random() * 100) + '%' + ', 50%)';
 
 var renderCloud = function (ctx, x, y, color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 var getMaxElement = function (arr) {
-    var maxElement = arr[0];
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] > maxElement) {
-            maxElement = arr[i];
-        }
+  var maxElement = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
     }
-    return maxElement;
+  }
+  return maxElement;
 };
 
-var getMessage = function (ctx, arr) {
-    ctx.fillStyle = TEXT_COLOR;
-    ctx.font = TEXT_MESSAGE_STYLE;
-    for (var i = 0; i < messages.length; i++) {
+var getMessage = function (ctx) {
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.font = TEXT_MESSAGE_STYLE;
+  for (var i = 0; i < messages.length; i++) {
     ctx.fillText(messages[i], CLOUD_WIDTH / 2, TEXT_HEIGHT * (i + 1));
-    }
+  }
 };
 
 var getBar = function (ctx, names, times) {
-    var maxTime = getMaxElement(times);
-    for (var i = 0; i < names.length; i++) {
-        var barHeight = (MAX_BAR_HEIGHT * times[i]) / maxTime;
-        ctx.fillStyle = TEXT_COLOR;
-        ctx.fillText(names[i], CLOUD_X + BAR_GAP * (i + 1) + BAR_WIDTH * i, CLOUD_HEIGHT);
-        ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - TEXT_HEIGHT - barHeight - GAP);
-        if (names[i] === 'Вы') {
-            ctx.fillStyle = PLAYER_COLOR;
-        } else {
-            ctx.fillStyle = RANDOM_BLUE;
-        } 
-        ctx.fillRect(CLOUD_X + BAR_GAP * (i + 1) + BAR_WIDTH * i, CLOUD_HEIGHT - barHeight - TEXT_HEIGHT, BAR_WIDTH, barHeight);
+  var maxTime = getMaxElement(times);
+  for (var i = 0; i < names.length; i++) {
+    var barHeight = (MAX_BAR_HEIGHT * times[i]) / maxTime;
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(names[i], CLOUD_X + BAR_GAP * (i + 1) + BAR_WIDTH * i, CLOUD_HEIGHT);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - TEXT_HEIGHT - barHeight - GAP);
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = PLAYER_COLOR;
+    } else {
+      ctx.fillStyle = RANDOM_BLUE;
     }
+    ctx.fillRect(CLOUD_X + BAR_GAP * (i + 1) + BAR_WIDTH * i, CLOUD_HEIGHT - barHeight - TEXT_HEIGHT, BAR_WIDTH, barHeight);
+  }
 };
 
 window.renderStatistics = function (ctx, names, times) {
-    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_SHADOW_COLOR);
-    renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
-    getMessage(ctx, messages);
-    getBar(ctx, names, times);
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
+  getMessage(ctx, messages);
+  getBar(ctx, names, times);
 };
