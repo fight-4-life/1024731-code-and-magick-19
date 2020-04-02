161e9f1ca5a1c6c453c92af7d88264279d384d52
Vladimir Legotin <wovanza@gmail.com>
2020-04-02 12:56:33 +0500

    Opens setup window and shows the block with similar wizards

---
+++ js/setup.js
@@ -0,0 +1,12 @@
+
+'use strict';
+
+(function () {
+
+  // opening setup window
+  document.querySelector('.setup').classList.remove('hidden');
+
+  // showing the block with similar wizards
+  document.querySelector('.setup-similar').classList.remove('hidden');
+
+})();
